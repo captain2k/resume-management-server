57792b928fe5d9276fcf9ce5fac47f8c58d08563
captain2k <hiencn@global.com>
2022-10-12 17:31:55 +0700

    init feat:projects

---
+++ src/projects/entities/project.entity.ts
@@ -0,0 +1,10 @@
+import { Project } from '@prisma/client';
+
+export class ProjectEntity implements Project {
+  id: string;
+  title: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  duration: number;
+}
